b7b535c78fbf2de9da4d83b908b2da150b8d6b7f
TheHunterGolden <huntergolden0124@gmail.com>
2018-04-26 17:21:31 -0400

    touch to activate heads Vive + Oculus Support

---
+++ Assets/AnimHandler.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Playables;
+using VRTK;
+
+public class AnimHandler : MonoBehaviour {
+
+    private PlayableDirector director;
+	public bool close;
+	public PlayableDirector[] toAnimate;
+	//private bool isPaused;
+    
+	// Use this for initialization
+	void Awake() {
+		toAnimate = gameObject.GetComponentsInChildren<PlayableDirector> ();
+		director = gameObject.GetComponent<PlayableDirector>();
+		foreach (PlayableDirector dir in toAnimate) {
+			dir.playOnAwake = false;
+			dir.extrapolationMode = DirectorWrapMode.Loop;
+		}
+
+	}
+	void Start () {
+
+
+		//isPaused = false;
+		//toAnimate = gameObject.GetComponentsInChildren<PlayableDirector> ();
+        //director = gameObject.GetComponent<PlayableDirector>();
+		foreach (PlayableDirector dir in toAnimate) {
+			//dir.playOnAwake = false;
+			//dir.extrapolationMode = DirectorWrapMode.Loop;
+		}
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		if (close) {
+			foreach (PlayableDirector dir in toAnimate) {
+				director.enabled = true;
+				director.Play ();
+					}
+		} else if (!close) {
+			foreach (PlayableDirector dir in toAnimate) {
+				//director.enabled = false;
+				director.Pause ();
+
+			}
+			}
+	}
+
+	void OnTriggerEnter (Collider col) {
+		//Debug.Log ("Entered: " + col.gameObject.name);
+		close = true;
+	}
+
+	void OnTriggerExit (Collider col) {
+		//Debug.Log ("Exited: " + col.gameObject.name);
+		close = false;
+	}
+}
---
+++ Assets/detectObject.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using VRTK;
+
+public class detectObject : MonoBehaviour {
+
+	public GameObject touching;
+
+	// Use this for initialization
+	void Start () {
+		touching = gameObject.GetComponent<VRTK_InteractTouch> ().GetTouchedObject ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (touching != null) {
+			touching = gameObject.GetComponent<VRTK_InteractTouch> ().GetTouchedObject ();
+			Debug.Log (touching.name);
+		}
+	}
+}
